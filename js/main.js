fc8bcfe62f34ebce7b51e0202c7ae9f8bba6841c
Dany Hawk <dhawk@rochester.rr.com>
2018-03-29 00:25:26 -0400

    Added beautiful series project page

--- js/main.js
+++ js/main.js
@@ -162,15 +162,17 @@ function createHTMLEvents() {
     var purple = unhex(["88", "00", "88"]);
 
     document.querySelector("#narwhal").addEventListener("mouseenter", function() {spawnCircle(red)});
-    document.querySelector("#boop").addEventListener("mouseenter", function() {spawnCircle(orange)});
-    document.querySelector("#dog").addEventListener("mouseenter", function() {spawnCircle(yellow)});
-    document.querySelector("#rocks").addEventListener("mouseenter", function() {spawnCircle(green)});
+    document.querySelector("#beautiful").addEventListener("mouseenter", function() {spawnCircle(orange)});
+    document.querySelector("#boop").addEventListener("mouseenter", function() {spawnCircle(yellow)});
+    document.querySelector("#dog").addEventListener("mouseenter", function() {spawnCircle(green)});
+    document.querySelector("#rocks").addEventListener("mouseenter", function() {spawnCircle(blue)});
     //document.querySelector("#todo").addEventListener("mouseenter", function() {spawnCircle(blue)});
-    document.querySelector("#montague").addEventListener("mouseenter", function() {spawnCircle(blue)});
+    document.querySelector("#montague").addEventListener("mouseenter", function() {spawnCircle(purple)});
 
 
     document.querySelector("#boop").addEventListener("click", function() {window.location.href="./boop/index.html"});
     document.querySelector("#narwhal").addEventListener("click", function() {window.location.href="./narwhal.html"});
+    document.querySelector("#beautiful").addEventListener("click", function() {window.location.href="./beautiful.html"});
     document.querySelector("#dog").addEventListener("click", function() {window.location.href="./dog.html"});
     document.querySelector("#rocks").addEventListener("click", function() {window.location.href="./litrocks.html"});
     //document.querySelector("#todo").addEventListener("click", function() {window.location.href="http://puppyproductivity.club"});
