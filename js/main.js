fcc61935bb13c351228e3704bc329d988a5d32dd
Dany Hawk <dhawk@rochester.rr.com>
2018-05-17 12:01:51 -0400

    Changed the homepage color scheme

--- js/main.js
+++ js/main.js
@@ -154,20 +154,50 @@ function createBounds() {
 
 //Called at setup to link canvas events to html Elements
 function createHTMLEvents() {
-    var red = unhex(["FE", "00", "00"]);
-    var orange = unhex(["FF", "88", "00"]);
-    var yellow = unhex(["FD", "FE", "02"]);
-    var green = unhex(["0B", "FF", "01"]);
-    var blue = unhex(["01", "1E", "FE"]);
-    var purple = unhex(["88", "00", "88"]);
+    var red = unhex(["FE", "00", "00"]); //FE0000
+    var orange = unhex(["FF", "88", "00"]); //FF8800
+    var yellow = unhex(["FD", "FE", "02"]); //FDFE02
+    var green = unhex(["0B", "FF", "01"]); //0BFF01
+    var blue = unhex(["01", "1E", "FE"]); //011EFE
+    var purple = unhex(["88", "00", "88"]); //880088
 
-    document.querySelector("#narwhal").addEventListener("mouseenter", function() {spawnCircle(red)});
-    document.querySelector("#boop").addEventListener("mouseenter", function() {spawnCircle(orange)});
-    document.querySelector("#beautiful").addEventListener("mouseenter", function() {spawnCircle(yellow)});
-    document.querySelector("#dog").addEventListener("mouseenter", function() {spawnCircle(green)});
-    document.querySelector("#rocks").addEventListener("mouseenter", function() {spawnCircle(blue)});
+    var palette11 = unhex(["D4", "00", "4F"]); //d4004f
+    var palette12 = unhex(["48", "4E", "A5"]); //484ea5
+    var palette13 = unhex(["00", "70", "A7"]); //0070a7
+    var palette14 = unhex(["0A", "91", "90"]); //0a9190
+    var palette15 = unhex(["F8", "D7", "39"]); //f8d739
+
+    var palette21 = unhex(["FF", "9D", "DF"]); //ff9ddf
+    var palette22 = unhex(["89", "28", "BE"]); //8928be
+    var palette23 = unhex(["99", "D5", "F7"]); //99d5f7
+    var palette24 = unhex(["91", "FD", "6A"]); //91fd6a
+    var palette25 = unhex(["CE", "F6", "B7"]); //cef6b7
+
+    var palette31 = unhex(["4C", "ED", "C5"]); //4cedc5
+    var palette32 = unhex(["FE", "B4", "DF"]); //feb4df
+    var palette33 = unhex(["6A", "85", "F7"]); //6a85f7
+    var palette34 = unhex(["00", "CA", "FA"]); //00cafa
+    var palette35 = unhex(["00", "EE", "FD"]); //00eefd
+
+    var palette41 = unhex(["EF", "00", "6C"]); //ef006c
+    var palette42 = unhex(["FF", "2A", "90"]); //ff2a90
+    var palette43 = unhex(["F7", "CC", "44"]); //f7cc44
+    var palette44 = unhex(["E9", "6A", "3C"]); //e96a3c
+    var palette45 = unhex(["E9", "3C", "2C"]); //e93c2c
+
+    var palette51 = unhex(["2A", "7F", "7D"]); //2a7f7d
+    var palette52 = unhex(["D1", "00", "5F"]); //d1005f
+    var palette53 = unhex(["49", "66", "A6"]); //4966a6
+    var palette54 = unhex(["E3", "99", "59"]); //e39959
+    var palette55 = unhex(["DD", "D4", "C8"]); //ddd4c8
+
+    document.querySelector("#narwhal").addEventListener("mouseenter", function() {spawnCircle(palette51)});
+    document.querySelector("#boop").addEventListener("mouseenter", function() {spawnCircle(palette52)});
+    document.querySelector("#beautiful").addEventListener("mouseenter", function() {spawnCircle(palette53)});
+    document.querySelector("#dog").addEventListener("mouseenter", function() {spawnCircle(palette54)});
+    document.querySelector("#rocks").addEventListener("mouseenter", function() {spawnCircle(palette55)});
     //document.querySelector("#todo").addEventListener("mouseenter", function() {spawnCircle(blue)});
-    document.querySelector("#montague").addEventListener("mouseenter", function() {spawnCircle(purple)});
+    //document.querySelector("#montague").addEventListener("mouseenter", function() {spawnCircle(purple)});
 
 
     document.querySelector("#boop").addEventListener("click", function() {window.location.href="./boop.html"});
@@ -176,5 +206,5 @@ function createHTMLEvents() {
     document.querySelector("#dog").addEventListener("click", function() {window.location.href="./dog.html"});
     document.querySelector("#rocks").addEventListener("click", function() {window.location.href="./litrocks.html"});
     //document.querySelector("#todo").addEventListener("click", function() {window.location.href="http://puppyproductivity.club"});
-    document.querySelector("#montague").addEventListener("click", function() {window.location.href="./montague.html"});
+    //document.querySelector("#montague").addEventListener("click", function() {window.location.href="./montague.html"});
 }
