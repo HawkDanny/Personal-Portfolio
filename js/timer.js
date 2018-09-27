5e0431f54eb5959bd3737a925a29e35479bf64b0
Danny Hawk <HawkDanny@users.noreply.github.com>
2018-09-26 21:42:25 -0400

    Fixed typo

--- js/timer.js
+++ js/timer.js
@@ -38,7 +38,7 @@ function setup() {
         skip();   
     });
     canvas.addEventListener("click", skip);
-    window.screen.addEventListener("orientationchange", resize);
+    window.addEventListener("orientationchange", resize);
 
     //Starting background color
     backgroundColor = menuBackgroundColor;
