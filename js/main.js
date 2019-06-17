d1b32dae9db6a8b288342e3d257a5c66a4e68f76
Danny Hawk <hello@dannyhawk.com>
2019-06-17 12:52:47 -0400

    Portfolio v5 has officially been replaced by v6

--- js/main.js
+++ js/main.js
@@ -1,210 +1,118 @@
-//Aliases
-var Engine = Matter.Engine,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Body = Matter.Body,
-    Composite = Matter.Composite,
-    Vertices = Matter.Vertices;
+window.onload = init;
+window.onscroll = updateNav;
+var scrollItems;
+var scrollSelectedItems;
+var scrollEvents;
+var firstSetup = true;
+var scrollIndex = 0; //index of the current scrollItem being looked at by the user
 
-//Global engine reference
-var engine;
-var world;
-var floor;
+var activeNavStyle = "color: var(--color-header); text-shadow: 1px 1px var(--color-fade2);";
+var inactiveNavStyle = "color: var(--color-detail);";
 
-var objects = [];
+var complete = true;
 
-//DOM element
-var article;
-var scrollPos;
+function init() {
+    var fitties = fitty('.fit', {
+    minSize: 12,
+    maxSize: 92
+    });
 
-//Cards
-var characterCard;
-var eventCard;
-var willCard;
-
-//Instagram
-var rockPic;
-
-//Sidebar
-var isSidebarExpanded;
-
-function setup() {
-    article = document.querySelector(".content");
-    scrollPos = 0;
-
-    //Make the canvas and tell p5 about it
-    var cnv = createCanvas(window.innerWidth, window.innerHeight);
-    cnv.parent("sketch");
-
-    //Create the physics engine
-    engine = Engine.create();
-    world = engine.world;
-
-    createBounds();
-
-    createHTMLEvents();
-
-    //Start the simulation that runs at 60 frames per second
-    Engine.run(engine);
-}
-
-//Called every frame
-function draw() {
-    background(unhex(["FD", "FD", "FD"]));
-    noStroke();
-
-    for (var i = 0; i < objects.length; i++) {
-        objects[i].show();
-
-
-        //If an object manages to get off screen, delete it to keep processing light
-        if (objects[i].isOffScreen()) {
-            objects[i].removeFromWorld();
-            objects.splice(i, 1);
-            --i;
-        }
+    for (var i = 0; i < fitties.length; i++)
+    {
+        fitties[0].fit();
     }
 
-    if (scrollPos !== article.scrollTop) {
+    getScrollItems();
+}
 
-        translateCanvas(scrollPos - article.scrollTop);
+//Called every time the window is scrolled
+function updateNav() {
+    if (document.getElementById(scrollSelectedItems[scrollIndex].dataset.sisterTag).getBoundingClientRect().y < window.innerHeight / 2)
+    {
+        scrollSelectedItems[scrollIndex].style = activeNavStyle;
+        if (scrollIndex > 0)
+            scrollSelectedItems[scrollIndex-1].style = inactiveNavStyle;
         
-        scrollPos = article.scrollTop;
+        scrollIndex = Math.min(scrollIndex + 1, scrollSelectedItems.length - 1);
+    } else if (scrollIndex > 0 && document.getElementById(scrollSelectedItems[scrollIndex - 1].dataset.sisterTag).getBoundingClientRect().y >= 0)
+    {
+        scrollSelectedItems[scrollIndex - 1].style = activeNavStyle;
+        scrollSelectedItems[scrollIndex].style = inactiveNavStyle;
+        scrollIndex = Math.max(scrollIndex - 1, 0);
+    } else if (scrollIndex === 0)
+    {
+        scrollSelectedItems[0].style = inactiveNavStyle;
     }
 }
 
-function spawnCircle(color) {
-    var pos = randomSpawn(25);
-    objects.push(new Circle(pos.x, pos.y, 50, color)); 
-}
+//Get all the scrollItems
+function getScrollItems() {
+    scrollItems = document.getElementsByClassName("sideBarClickable");
+    scrollSelectedItems = new Array();
 
-function boostUpward(force) {
-    for (var i = 0; i < objects.length; i++) {
-        //Don't boost it if it isn't in contact with the floor
-        if (objects[i].body.position.y < window.innerHeight - objects[i].radius){
-            continue;
+    //remove the previous eventListeners
+    if (!firstSetup)
+    {
+        for (var i = 0; i < scrollItems.length; i++)
+        {
+            scrollItems[i].removeEventListener("click", scrollEvents[i], true);
         }
-        objects[i].applyForce(objects[i].body.position, {x: 0, y: force});
-    }
-}
-
-function translateCanvas(yTranslation) {
-    
-    //Prevent the objects from tunneling, by keeping yTranslation between -90 and 90
-    yTranslation = Math.min(yTranslation, 75)
-    yTranslation = Math.max(yTranslation, -75);
-
-    if (yTranslation > 0) {
-        boostUpward(yTranslation * -0.005);
     }
 
-    Composite.translate(world, {x: 0, y: yTranslation});
+    scrollEvents = new Array(scrollItems.length);
 
-    resetBorders();
-}
+    for (var i = 0; i < scrollItems.length; i++)
+    {
+        if (!complete && !scrollItems[i].classList.contains('selected'))
+            continue;
+        
+        scrollItems[i].addEventListener("click", (scrollEvents[i] = function(j) {
+        return function() {
+            document.getElementById(scrollItems[j].dataset.sisterTag).scrollIntoView({behavior: 'smooth'});
+        }
+        })(i));
 
-//A function that returns a random spawn point in the top half of the canvas
-function randomSpawn(buffer) {
-    var randX = random(buffer, width - buffer);
-    var randY = random(buffer, (height / 2) - buffer);
-
-    return {
-        x: randX,
-        y: randY
+        scrollSelectedItems[scrollSelectedItems.length] = scrollItems[i];
     }
+
+    firstSetup = false;
 }
 
-//Resize the canvas and reset the boundaries on window resize
-function windowResized() {
-    resizeCanvas(window.innerWidth, window.innerHeight);
-
-    World.remove(world, leftWall);
-    World.remove(world, rightWall);
-    World.remove(world, floor);
-    World.remove(world, ceiling);
-
-    createBounds();
+function scrollTo(element, id) {
+    scrollItems[scrollItems.length - 1] = id;
+    console.log(scrollItems[scrollItems.length - 2]);
+    //document.getElementById(id).scrollIntoView({behavior: 'smooth'});
 }
 
-function resetBorders() {
+function toggleComplete() {
+    document.getElementById("completeText").className = "active";
+    document.getElementById("selectedText").className = "inactive";
 
-    World.remove(world, leftWall);
-    World.remove(world, rightWall);
-    World.remove(world, floor);
-    World.remove(world, ceiling);
+    complete = !complete;
+    var completeItems = document.getElementsByClassName('complete');
 
-    createBounds();
+    for (var i = 0; i < completeItems.length; i++)
+    {
+        completeItems[i].style.display = 'block';
+    }
+
+    getScrollItems();
 }
 
-//Create the floor and walls out of static rectangles
-function createBounds() {
-    //Floor
-    floor = Bodies.rectangle(width / 2, height + 50, width, 100, {isStatic: true});
+function toggleSelected() {
+    document.getElementById("completeText").className = "inactive";
+    document.getElementById("selectedText").className = "active";
 
-    //Ceiling
-    ceiling = Bodies.rectangle(width / 2, -50, width, 100, {isStatic: true});
+    complete = !complete;
+    var completeItems = document.getElementsByClassName('complete');
 
-    //Walls
-    leftWall = Bodies.rectangle(-10, height / 2, 20, height + 40, {isStatic: true});
-    rightWall = Bodies.rectangle(width + 10, height / 2, 20, height + 40, {isStatic: true});
+    for (var i = 0; i < completeItems.length; i++)
+    {
+        if (completeItems[i].classList.contains('selected'))
+            continue;
+        
+        completeItems[i].style.display = 'none';
+    }
 
-    World.add(world, [floor, ceiling, leftWall, rightWall]);
-}
-
-
-//Called at setup to link canvas events to html Elements
-function createHTMLEvents() {
-    var red = unhex(["FE", "00", "00"]); //FE0000
-    var orange = unhex(["FF", "88", "00"]); //FF8800
-    var yellow = unhex(["FD", "FE", "02"]); //FDFE02
-    var green = unhex(["0B", "FF", "01"]); //0BFF01
-    var blue = unhex(["01", "1E", "FE"]); //011EFE
-    var purple = unhex(["88", "00", "88"]); //880088
-
-    var palette11 = unhex(["D4", "00", "4F"]); //d4004f
-    var palette12 = unhex(["48", "4E", "A5"]); //484ea5
-    var palette13 = unhex(["00", "70", "A7"]); //0070a7
-    var palette14 = unhex(["0A", "91", "90"]); //0a9190
-    var palette15 = unhex(["F8", "D7", "39"]); //f8d739
-
-    var palette21 = unhex(["FF", "9D", "DF"]); //ff9ddf
-    var palette22 = unhex(["89", "28", "BE"]); //8928be
-    var palette23 = unhex(["99", "D5", "F7"]); //99d5f7
-    var palette24 = unhex(["91", "FD", "6A"]); //91fd6a
-    var palette25 = unhex(["CE", "F6", "B7"]); //cef6b7
-
-    var palette31 = unhex(["4C", "ED", "C5"]); //4cedc5
-    var palette32 = unhex(["FE", "B4", "DF"]); //feb4df
-    var palette33 = unhex(["6A", "85", "F7"]); //6a85f7
-    var palette34 = unhex(["00", "CA", "FA"]); //00cafa
-    var palette35 = unhex(["00", "EE", "FD"]); //00eefd
-
-    var palette41 = unhex(["EF", "00", "6C"]); //ef006c
-    var palette42 = unhex(["FF", "2A", "90"]); //ff2a90
-    var palette43 = unhex(["F7", "CC", "44"]); //f7cc44
-    var palette44 = unhex(["E9", "6A", "3C"]); //e96a3c
-    var palette45 = unhex(["E9", "3C", "2C"]); //e93c2c
-
-    var palette51 = unhex(["2A", "7F", "7D"]); //2a7f7d
-    var palette52 = unhex(["D1", "00", "5F"]); //d1005f
-    var palette53 = unhex(["49", "66", "A6"]); //4966a6
-    var palette54 = unhex(["E3", "99", "59"]); //e39959
-    var palette55 = unhex(["DD", "D4", "C8"]); //ddd4c8
-
-    document.querySelector("#narwhal").addEventListener("mouseenter", function() {spawnCircle(palette51)});
-    document.querySelector("#boop").addEventListener("mouseenter", function() {spawnCircle(palette52)});
-    document.querySelector("#beautiful").addEventListener("mouseenter", function() {spawnCircle(palette53)});
-    document.querySelector("#dog").addEventListener("mouseenter", function() {spawnCircle(palette54)});
-    document.querySelector("#rocks").addEventListener("mouseenter", function() {spawnCircle(palette55)});
-    //document.querySelector("#todo").addEventListener("mouseenter", function() {spawnCircle(blue)});
-    //document.querySelector("#montague").addEventListener("mouseenter", function() {spawnCircle(purple)});
-
-
-    document.querySelector("#boop").addEventListener("click", function() {window.location.href="./boop.html"});
-    document.querySelector("#narwhal").addEventListener("click", function() {window.location.href="./narwhal.html"});
-    document.querySelector("#beautiful").addEventListener("click", function() {window.location.href="./beautiful.html"});
-    document.querySelector("#dog").addEventListener("click", function() {window.location.href="./dog.html"});
-    document.querySelector("#rocks").addEventListener("click", function() {window.location.href="./litrocks.html"});
-    //document.querySelector("#todo").addEventListener("click", function() {window.location.href="http://puppyproductivity.club"});
-    //document.querySelector("#montague").addEventListener("click", function() {window.location.href="./montague.html"});
+    getScrollItems();
 }
