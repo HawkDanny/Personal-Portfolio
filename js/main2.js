d1b5e6d1f478ed20ae2a17a6d7ce4eb439d737cd
Danny Hawk <hello@dannyhawk.com>
2020-06-04 20:28:15 -0400

    Completed style workovers

--- js/main2.js
+++ js/main2.js
@@ -1,27 +1,89 @@
 window.onload = init;
 
+let root;
+
 let dohideElements;
 let filters;
 
 let everything;
+let everythingSide;
 
 let projBorder = "solid black 4px";
 let projBorderInvert = "solid white 4px";
 
+let years = {};
 
 function init() {
+    root = document.documentElement;
     dohideElements = document.querySelectorAll(".dohide");
     filters = document.querySelectorAll(".filter");
     everything = document.querySelectorAll(".ev");
+    everythingSide = document.querySelectorAll(".ev-s");
+    
+    //sidebar year headers
+    years.y2015 = document.querySelector(".y2015");
+    years.y2016 = document.querySelector(".y2016");
+    years.y2017 = document.querySelector(".y2017");
+    years.y2018 = document.querySelector(".y2018");
+    years.y2019 = document.querySelector(".y2019");
+    years.y2020 = document.querySelector(".y2020");
+
+    //sidebar year projects
+    years.y2015s = document.querySelectorAll(".y15");
+    years.y2016s = document.querySelectorAll(".y16");
+    years.y2017s = document.querySelectorAll(".y17");
+    years.y2018s = document.querySelectorAll(".y18");
+    years.y2019s = document.querySelectorAll(".y19");
+    years.y2020s = document.querySelectorAll(".y20");
+
+    let url = new URL(window.location.href);
+    if (url.hash !== "")
+        filterFromURL(url.hash);
+    else
+        filter("hl");
+
 }
 
-function filter(clss, elmnt) {
+function filterFromURL(hash) {
+    switch (hash) {
+        case "#highlights":
+            filter('hl');
+        break;
+        case "#everything":
+            filter('ev');
+        break;
+        case "#videogames":
+            filter('vg');
+        break;
+        case "#analoggames":
+            filter('ag');
+        break;
+        case "#physicalcomputing":
+            filter('pc');
+        break;
+        case "#events":
+            filter('evn');
+        break;
+        case "#talks":
+            filter('ta');
+        break;
+        case "#videos":
+            filter('vi');
+        break;
+    }
+}
+
+function filter(clss) {
     //change the filter text styling
     for (let i = 0; i < filters.length; i++) {
         filters[i].classList.remove("filterActive");
     }
-    elmnt.classList.add("filterActive");
+    document.getElementById(clss).classList.add("filterActive");
 
+    filterProjects(clss);
+}
+
+function filterProjects(clss) {
     for (let i = 0; i < everything.length; i++) {
         if (everything[i].classList.contains(clss)) {
             everything[i].style.display = "block";
@@ -29,6 +91,38 @@ function filter(clss, elmnt) {
         else
             everything[i].style.display = "none";
     }
+
+    for (let i = 0; i < everythingSide.length; i++) {
+        if (everythingSide[i].classList.contains(clss + "-s")) {
+            everythingSide[i].style.display = "block";
+        }
+        else
+            everythingSide[i].style.display = "none";
+    }
+
+
+    //sidebar
+    filterSidebarYear(years.y2015, years.y2015s);
+    filterSidebarYear(years.y2016, years.y2016s);
+    filterSidebarYear(years.y2017, years.y2017s);
+    filterSidebarYear(years.y2018, years.y2018s);
+    filterSidebarYear(years.y2019, years.y2019s);
+    filterSidebarYear(years.y2020, years.y2020s);
+}
+
+function filterSidebarYear(year, years) {
+    let hide = false;
+    for (let i = 0; i < years.length; i++) {
+        if (years[i].style.display !== "none") {
+            hide = true;
+            break;
+        }
+    }
+
+    if (hide)
+        year.style.display = "block";
+    else
+        year.style.display = "none";
 }
 
 function FadeInBackground(background, elmnt)
@@ -41,6 +135,7 @@ function FadeInBackground(background, elmnt)
 
     if (!elmnt.classList.contains("projectInvert")) {
         elmnt.style.border = projBorderInvert;
+        root.style.setProperty("--color-border", "#FFFFFF");
     }
 
     switch (background) {
@@ -80,6 +175,9 @@ function FadeInBackground(background, elmnt)
         case 'folk':
             document.body.style.backgroundImage = "url('media/homepage/folk_homepage.jpg')";
             break;
+        case 'tabs':
+            document.body.style.backgroundImage = "url('media/homepage/tabs_homepage.jpg')";
+            break;
     }
 }
 
@@ -92,5 +190,6 @@ function FadeOutBackground(elmnt) {
 
     if (!elmnt.classList.contains("projectInvert")) {
         elmnt.style.border = projBorder;
+        root.style.setProperty("--color-border", "#000000");
     }
 }
