ea5c67f1c2f1771f0660c3621a5143eb29f36ff6
Danny Hawk <HawkDanny@users.noreply.github.com>
2023-04-06 23:09:54 -0400

    Populated data.js with more projects, built the foundations of the styling and the dom manipulation

--- data.js
+++ data.js
@@ -1,16 +1,238 @@
 let data = {
     projects: [
         {
-            name: "TestProj",
-            shortname: "test proj",
+            name: "GAMER POTLUCK",
             year: 2023,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+            logline: "Weekly livestream playing curated games",
+            role: "co-host",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
         },
         {
-            name: "TestProj2",
-            shortname: "test proj 2",
+            name: "Bird Town",
+            year: 2023,
+            logline: "A comedy video game about a town of birds",
+            role: "developer, designer, art director",
+            headerImageURL: "media/homepage/birdtown_homepage.png",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Good Sex",
             year: 2022,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+            logline: "\"Oh yeah, that's it. Keep doing what you're doing.\"",
+            role: "creator",
+            headerImageURL: "media/homepage/goodsex_homepage.png",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Social Media Games - Play and Make",
+            year: 2021,
+            logline: "How to create tiktok games",
+            role: "co-speaker",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Folk Game TikToks",
+            year: 2021,
+            logline: "In conversation with Now Play This",
+            role: "speaker",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "How To Be Born",
+            year: 2021,
+            logline: "Downloadable interactive tutorial",
+            role: "creator",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Itch.io Monday",
+            year: 2021,
+            logline: "A weekly streaming playing through experimental games from itch.io",
+            role: "host",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Folk Games, Flash Communities, and TikTok",
+            year: 2021,
+            logline: "An essay on how folk games have adapted to an algorithmic world",
+            role: "author",
+            headerImageURL: "media/homepage/newrules_homepage.png",
+            invertTitleColor: true,
+            description: ""
+        },
+        {
+            name: "Saxophone Skateboard",
+            year: 2020,
+            logline: "Improv jazz over falls",
+            role: "composer, performer",
+            headerImageURL: "media/homepage/sax_homepage.jpg",
+            invertTitleColor: true,
+            description: ""
+        },
+        {
+            name: "Folk Game Game Jam",
+            year: 2020,
+            logline: "DIY folk games",
+            role: "co-organizer",
+            headerImageURL: "media/homepage/folk_homepage.jpg",
+            invertTitleColor: true,
+            description: ""
+        },
+        {
+            name: "Anything But Games",
+            year: 2020,
+            logline: "An open mic series about everything else",
+            role: "organizer",
+            headerImageURL: "media/homepage/abg_homepage.jpg",
+            invertTitleColor: true,
+            description: ""
+        },
+        {
+            name: "New Trees",
+            year: 2020,
+            logline: "A story of passing time",
+            role: "developer, designer, composer",
+            headerImageURL: "media/homepage/newtrees_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Three Tabs",
+            year: 2020,
+            logline: "painful productivity",
+            role: "creator",
+            headerImageURL: "media/homepage/tabs_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Court",
+            year: 2019,
+            logline: "A card game for a deck of cards",
+            role: "co-designer",
+            headerImageURL: "media/homepage/court_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Imagine",
+            year: 2019,
+            logline: "Think about spring",
+            role: "creator",
+            headerImageURL: "media/homepage/imagine_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Perfect Ramen",
+            year: 2018,
+            logline: "Cooperative chopsticks simulator",
+            role: "co-creator",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Dam It!",
+            year: 2018,
+            logline: "A realtime strategy party game",
+            role: "co-creator",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "lit.rocks",
+            year: 2018,
+            logline: "A collection of virtual rocks",
+            role: "creator",
+            headerImageURL: "media/homepage/rock_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Danny Hawk's Boop Game",
+            year: 2018,
+            logline: "The game where you share and compete",
+            role: "creator",
+            headerImageURL: "media/homepage/boop2_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Beautiful Series",
+            year: 2018,
+            logline: "Three interactive artworks about beauty",
+            role: "creator",
+            headerImageURL: "media/homepage/beautiful_homepage.jpg",
+            invertTitleColor: true,
+            description: ""
+        },
+        {
+            name: "Gojira",
+            year: 2018,
+            logline: "An interactive comparison of Godzilla (1954) and Godzilla (1956)",
+            role: "creator",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Narwhal Picnic",
+            year: 2017,
+            logline: "Put a vive controller on your forehead",
+            role: "creator",
+            headerImageURL: "media/homepage/narwhal_homepage.jpg",
+            invertTitleColor: true,
+            description: ""
+        },
+        {
+            name: "My Dog's Eyes",
+            year: 2017,
+            logline: "A love letter to my favorite song",
+            role: "developer and designer, song from Zammuto",
+            headerImageURL: "media/homepage/dog_homepage.jpg",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Boop 1",
+            year: 2016,
+            logline: "A competitive game where you share the spacebar",
+            role: "creator",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "The Death of Mr. Montague",
+            year: 2016,
+            logline: "A card game about making up extravagant lies",
+            role: "co-designer",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
+        },
+        {
+            name: "Switch Blade",
+            year: 2015,
+            logline: "My first video game",
+            role: "creator",
+            headerImageURL: "",
+            invertTitleColor: false,
+            description: ""
         },
     ]
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,58 @@
 window.onload = init;
 
 function init() {
+    createTimeline();
+    createContent();
+
     console.log(data.projects.length);
+}
+
+function createTimeline() {
+    let tl = document.querySelector("#timeline");
+    let currentYear;
+    for (let i = 0; i < data.projects.length; i++) {
+        //create a year if it's a new one
+        if (data.projects[i].year != currentYear) {
+            currentYear = data.projects[i].year;
+            let year = document.createElement('p');
+            year.textContent = currentYear;
+            year.className = "year";
+            tl.appendChild(year);
+        }
+
+        let proj = document.createElement('p');
+        proj.textContent = data.projects[i].name;
+        tl.appendChild(proj);
+    }
+}
+
+function createContent() {
+    let cont = document.querySelector("#content");
+
+    for (let i = 0; i < data.projects.length; i++) {
+
+        //create item
+        let item = document.createElement('div');
+        item.className = "item";
+        item.style.backgroundImage = "url('" + data.projects[i].headerImageURL + "')";
+        if (data.projects[i].invertTitleColor)
+            item.style.color = "white";
+
+        //create itemText
+        let itemText = document.createElement('div');
+        itemText.className = "itemText";
+        item.appendChild(itemText);
+
+        //create title
+        let title = document.createElement("h1");
+        title.innerText = data.projects[i].name.toUpperCase();
+        itemText.appendChild(title);
+
+        //create logline
+        let logline = document.createElement("p");
+        logline.innerText = data.projects[i].logline;
+        itemText.appendChild(logline);
+
+        cont.appendChild(item);
+    }
 }
